d4fe0ad634aa4f52593ad4bbf494ca959f542049
Rajendra Khope <research@iocare.in>
2024-07-21 20:36:34 +0530

    Fix: library updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# TempVU" 
+# TempVU - Universal temperature sensor interface board
+
+TempVU is an universal temperature sensor module interface board for Sonpakhru board
---
+++ software/TempVU/src/main.cpp
@@ -0,0 +1,22 @@
+// Refer this : https://github.com/adafruit/Adafruit_MAX31865 - RTD-to-Digital Converter
+// Refer this: https://github.com/adafruit/Adafruit_MAX31856 - Thermocouple
+#include <Arduino.h>
+#include <Adafruit_SPIDevice.h>
+#include <Adafruit_MAX31865.h> // RTD-to-Digital Converter
+#include <Adafruit_MAX31856.h> // Thermocouple
+
+void setup()
+{
+  // Setup sensor and interfaces
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y)
+{
+  return x + y;
+}
